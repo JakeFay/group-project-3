3db610ab2fb530ac153279462e594655e4a363b5
Anthony Her <antpher@gmail.com>
2022-01-11 18:09:08 -0500

    state fixed

--- server/server.js
+++ server/server.js
@@ -30,16 +30,6 @@ app.use(express.json());
 // Serve up static assets
 app.use('/images', express.static(path.join(__dirname, '../client/images')));
 
-<<<<<<< HEAD
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
-if (process.env.NODE_ENV === 'production') {
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
-});
-}
-=======
 
 // if (process.env.NODE_ENV === 'production') {
 //   app.use(express.static(path.join(__dirname, '../client/build')));
@@ -50,7 +40,6 @@ app.get('*', (req, res) => {
 // });
 
 
->>>>>>> c71ef0c2a85cdeec01d373648f421dc9e68dfe38
 db.once('open', () => {
   app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`);
