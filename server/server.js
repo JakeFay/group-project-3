c731b2476c4cc086b7765d1bc958fce2b8033f2d
Anthony Her <antpher@gmail.com>
2022-01-10 19:49:00 -0500

    server starts

--- server/server.js
+++ server/server.js
@@ -14,7 +14,13 @@ const server = new ApolloServer({
   context: authMiddleware
 });
 
-server.applyMiddleware({ app });
+async function wait() {
+  await server.start(
+  )
+  server.applyMiddleware({ app })
+}
+
+wait();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
@@ -22,13 +28,13 @@ app.use(express.json());
 // Serve up static assets
 app.use('/images', express.static(path.join(__dirname, '../client/images')));
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static(path.join(__dirname, '../client/build')));
+// }
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
-});
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, '../client/build/index.html'));
+// });
 
 db.once('open', () => {
   app.listen(PORT, () => {
