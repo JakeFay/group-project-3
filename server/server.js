c4269add0c1677d987b5fa6167eb36d4329408ee
Jake <jake.fay9673@gmail.com>
2022-01-12 21:46:52 -0500

    pjson

--- server/server.js
+++ server/server.js
@@ -30,13 +30,13 @@ app.use(express.json());
 // Serve up static assets
 app.use('/images', express.static(path.join(__dirname, '../client/images')));
 
-// if (process.env.NODE_ENV === 'production') {
-//  app.use(express.static(path.join(__dirname, '../client/build')));
-// }
-// if (process.env.NODE_ENV === 'production') {
-// app.get('*', (req, res) => {
-//  res.sendFile(path.join(__dirname, '../client/build/index.html'));
-// });
+if (process.env.NODE_ENV === 'production') {
+ app.use(express.static(path.join(__dirname, '../client/build')));
+}
+if (process.env.NODE_ENV === 'production') {
+app.get('*', (req, res) => {
+ res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 db.once('open', () => {
   app.listen(PORT, () => {
